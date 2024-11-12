8b26e6bf437f78b5b864dc10d6c4e9781efac013
anthony74742 <antho230104@gmail.com>
2024-11-12 15:07:48 +0100

    Commit exercise

--- app/src/main/java/com/gmail/eamosse/tp2/LocalStudentManager.kt
+++ app/src/main/java/com/gmail/eamosse/tp2/LocalStudentManager.kt
@@ -1,7 +1,7 @@
 package com.gmail.eamosse.tp2
 
 
-class LocalStudentManager : StudentManager {
+class LocalStudentManager(var students: List<Student>) : StudentManager {
 // TODO 1. Implement all methods so that the class is conformed to the interface
     // TODO 2: init the class with a list of 10 students every time a new instance is created;
     // Use the followi
@@ -20,4 +20,83 @@ class LocalStudentManager : StudentManager {
     Etudiant(name = "John10", code = "A10", sexe = "F", address = "Lilles", age = 56),
     )
      */
+
+
+
+    /**
+     * A list of students
+     */
+    override fun listOf10(): List<Student> {
+        return students
+    }
+
+    /**
+     * Sort list
+     */
+    override fun sortAgeAscDesc(type: String): List<Student> {
+        if (type == "ASC") {
+            return students.sortedBy { it.age }
+        } else if (type == "DESC") {
+            return students.sortedByDescending { it.age }
+        } else {
+            return students
+        }
+    }
+
+    /**
+     * Group students by sex
+     */
+    override fun groupBySex(): Map<String, List<Student>> {
+        return students.groupBy { it.sexe }
+    }
+
+    /**
+     * Filter student by sex
+     */
+    override fun boysOrGirls(sexe: String): List<Student> {
+        return students.filter { it.sexe == sexe }
+    }
+
+    /**
+     * Get list of student names
+     */
+    override fun onlyNames(): List<String> {
+        return students.map { it.name }
+    }
+
+    /**
+     * Delete student by sex
+     */
+    override fun deleteBySex(sex: String) {
+        students = students.filter { it.sexe != sex }
+    }
+
+    /**
+     * Invert the list
+     */
+    override fun reverse(): List<Student> {
+        return students.reversed()
+    }
+
+    /**
+     * Clear the list
+     */
+    override fun clear() {
+        students = emptyList()
+    }
 }
+
+var students = listOf(
+    Student(name = "John1", code = "A1", sexe = "M", address = "Lilles", age = 20),
+    Student(name = "John2", code = "A2", sexe = "F", address = "Lilles", age = 30),
+    Student(name = "John3", code = "A3", sexe = "F", address = "Lilles", age = 41),
+    Student(name = "John4", code = "A4", sexe = "M", address = "Lilles", age = 42),
+    Student(name = "John5", code = "A5", sexe = "M", address = "Lilles", age = 34),
+    Student(name = "John6", code = "A6", sexe = "F", address = "Lilles", age = 12),
+    Student(name = "John7", code = "A7", sexe = "M", address = "Lilles", age = 28),
+    Student(name = "John8", code = "A8", sexe = "M", address = "Lilles", age = 16),
+    Student(name = "John9", code = "A9", sexe = "F", address = "Lilles", age = 10),
+    Student(name = "John10", code = "A10", sexe = "F", address = "Lilles", age = 56),
+)
+
+val studentManager = LocalStudentManager(students)
